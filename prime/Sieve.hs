6b4caab69b640e2962707e33dc53ff5a9f9c7a0a
Dimitri 'phaazon' Sabadie <dimitri.sabadie@gmail.com>
2015-10-11 19:59:21 +0200

    Correct sieve.

--- prime/Sieve.hs
+++ prime/Sieve.hs
@@ -1,31 +1,7 @@
 module Sieve where
 
-import Data.DList ( DList, empty, toList, snoc )
-
-data Sieve = Sieve (DList Int) [Maybe Int] deriving (Eq,Show)
-
-numbersUpTo :: Int -> [Maybe Int]
-numbersUpTo m
-  | m >= 2 = map Just [2..m]
-  | otherwise = []
-
-discriminate :: Sieve -> Sieve
-discriminate (Sieve primes nbs) = case nbs of
-  (Nothing:xs) -> discriminate $ Sieve primes xs
-  (Just p:_) -> discriminate $ Sieve (primes `snoc` p) (tail $ killEach p nbs)
-  _ -> Sieve primes []
-
-sieve :: Int -> Sieve
-sieve m = Sieve empty (numbersUpTo m)
-
--- TODO: non-tail recursive, might destroy stack!
-killEach :: (Ord a) => Int -> [Maybe a] -> [Maybe a]
-killEach m = go 1
-  where
-    go _ [] = []
-    go q (x:xs)
-      | q <= 1 = Nothing : go m xs
-      | otherwise = x : go (pred q) xs
-
 prime :: Int -> [Int]
-prime n = let Sieve primes _ = discriminate (sieve n) in toList primes
+prime n = sieve [2..n]
+  where
+    sieve (p:xs) = p : sieve (filter (\x -> x `rem` p /= 0) xs)
+    sieve _ = []
